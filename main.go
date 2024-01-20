9fbee9318f425929ebeca17cf121a385fc3f98aa
Natchaya Phongkuson <tpncy@MacBook-Air-khxng-Natchaya.local>
2024-01-20 15:03:40 +0700

    add module and server

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/NATCHAYATP/E-Commerce/config"
+	"github.com/NATCHAYATP/E-Commerce/modules/servers"
+	"github.com/Rayato159/kawaii-shop-tutorial/pkg/databases"
+)
+
+func envPath() string {
+	if len(os.Args) == 1 {
+		return ".env"
+	} else {
+		return os.Args[1]
+	}
+}
+
+func main() {
+	cfg := config.LoadConfig(envPath())
+
+	db := databases.DbConnect(cfg.Db())
+	defer db.Close()
+	fmt.Println(db)
+
+	servers.NewServer(cfg, db).Start()
+}
---
+++ pkg/databases/db.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"log"
+
+	"github.com/NATCHAYATP/E-Commerce/config"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/jmoiron/sqlx"
+)
+
+func DbConnect(cfg config.IDbConfig) *sqlx.DB {
+	// Connect
+	db, err := sqlx.Connect("pkg", cfg.Url())
+	if err != nil {
+		log.Fatalf("connect to db failed: %v\n", err)
+	}
+	db.DB.SetMaxOpenConns(cfg.MaxOpenConns())
+	return db
+}
